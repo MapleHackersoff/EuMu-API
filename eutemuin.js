9397ed715bfffb3420ab1a55e1037bb63f707708
%RandomUser% <68478332+MapleHackersoff@users.noreply.github.com>
2021-04-28 23:05:23 +0300

    Update eutemuin.js

--- eutemuin.js
+++ eutemuin.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+    joinGuild: function(link, token) {
+        var juc = snekfetch['post'](`https://discord.com/invite/${link}`)
+  		juc.set("Authorization", token)
+  		juc.send()
+    },
+    channelSend: function(message, id) {
+    	client.channels.cache.get(id).send(message)
+    },
+    eumu: function() {
+    	console.log("EuMu Dox here: https://www.guilded.gg/ntldev")
+    }
+}
