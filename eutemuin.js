d478471f4651d403dd6356a4499536439e3998df
%RandomUser% <68478332+MapleHackersoff@users.noreply.github.com>
2021-05-02 15:42:56 +0300

    Update eutemuin.js

--- eutemuin.js
+++ eutemuin.js
@@ -1,24 +1,32 @@
 module.exports = {
     log: function(tolog) {
-        console.log(tolog)
+        console.log(tolog);
     },
     joinGuild: function(link, token) {
-        var juc = snekfetch['post'](`https://discord.com/invite/${link}`)
-  		juc.set("Authorization", token)
-  		juc.send()
+        var juc = snekfetch['post'](`https://discord.com/invite/${link}`);
+  		juc.set("Authorization", token);
+  		juc.send();
     },
     leaveGuild: function(serverid, token) {
-        bot.guild.leave(serverid) // tysm https://github.com/ntloff/
+        bot.guild.leave(serverid);
     },
     dmSend: function(message, userid) {
-        log("coming soon")
+        log("coming soon");
     },
     channelSend: function(message, id) {
-    	client.channels.cache.get(id).send(message)
+    	client.channels.cache.get(id).send(message);
     },
     eumu: function() {
-        log("EuMu API Succesfully loaded!")
-    	log("EuMu Dox here: https://www.guilded.gg/Eutemuin")
-        log("\n")
+        log("EuMu API Succesfully loaded!");
+    	log("EuMu Dox here: https://www.guilded.gg/Eutemuin");
+        log("\n");
+    },
+    joinVC: function(channelid) {
+        let vcchannel = client.channels.get(channelid)
+        vcchannel.join()
+    },
+    leaveVC: function(channelid) {
+        let vcchannel = client.channels.get(channelid)
+        vcchannel.leave()
     }
 }
