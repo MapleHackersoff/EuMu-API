d4e3e0d8ae34f4ef32ef27fc5212ff84f9f4430a
%RandomUser% <68478332+MapleHackersoff@users.noreply.github.com>
2021-05-02 15:30:27 +0300

    Update eutemuin.js

--- eutemuin.js
+++ eutemuin.js
@@ -1,13 +1,24 @@
 module.exports = {
+    log: function(tolog) {
+        console.log(tolog)
+    },
     joinGuild: function(link, token) {
         var juc = snekfetch['post'](`https://discord.com/invite/${link}`)
   		juc.set("Authorization", token)
   		juc.send()
     },
+    leaveGuild: function(serverid, token) {
+        bot.guild.leave(serverid) // tysm https://github.com/ntloff/
+    },
+    dmSend: function(message, userid) {
+        log("coming soon")
+    },
     channelSend: function(message, id) {
     	client.channels.cache.get(id).send(message)
     },
     eumu: function() {
-    	console.log("EuMu Dox here: https://www.guilded.gg/ntldev")
+        log("EuMu API Succesfully loaded!")
+    	log("EuMu Dox here: https://www.guilded.gg/Eutemuin")
+        log("\n")
     }
 }
